474821281eb42e551c66f92b2d5eb9893ca479b4
Suraj Vishwakarma <suraj.vish55@gmail.com>
2020-04-16 01:01:26 +0530

    new yml code - emulator start code removed

--- app/src/main/java/com/example/surajvishwakarma/okhttp_android/MainScreen.java
+++ app/src/main/java/com/example/surajvishwakarma/okhttp_android/MainScreen.java
@@ -15,6 +15,9 @@ import okhttp3.Response;
 
 public class MainScreen extends AppCompatActivity {
 
+
+    String name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
